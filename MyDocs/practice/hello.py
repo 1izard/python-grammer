b9127412099860c1488a71899e6b917a31694353
1izard <guitar.kubittake@gmail.com>
2019-02-20 23:41:01 +0900

    rename hello.py and hello2.py to hello_package.py and hello_package2.py respectively

--- MyDocs/practice/hello.py
+++
@@ -1,6 +0,0 @@
-import Practice.hello2
-
-Practice.hello2
-Practice.hello2.print_hello2()
-hello2 = Practice.hello2.Hello2(1)
-print('hello2.value in hello.py =', hello2.value)
---
+++ MyDocs/practice/hello_package.py
@@ -0,0 +1,6 @@
+import practice.hello_package2
+
+practice.hello_package2
+practice.hello_package2.print_hello2()
+hello2 = practice.hello_package2.Hello2(1)
+print('hello2.value in hello.py =', hello2.value)
