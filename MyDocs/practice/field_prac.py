12f1accee217ceff17dde2834f65c96f9ca72038
1izard <guitar.kubittake@gmail.com>
2019-02-21 23:13:59 +0900

    add field_prac.py

---
+++ MyDocs/practice/field_prac.py
@@ -0,0 +1,38 @@
+class MyClass(object):
+    pass
+
+
+class MyClass2():
+    pass
+
+
+def main():
+    my_class = MyClass()
+    print('my_class.__dict__ =', my_class.__dict__)
+    print('my_class.__class__.__dict__ =', my_class.__class__.__dict__)
+    print()
+
+    # Though MyClass and the instance doesn't define field, my_class can use field as instance field.
+    my_class.field = 'One'
+    print('my_class.__dict__ =', my_class.__dict__)
+    print('my_class.__class__.__dict__ =', my_class.__class__.__dict__)
+    print()
+
+    # In Python 3, class inherit object class as default.
+    my_class = MyClass2()
+    print('my_class.__dict__ =', my_class.__dict__)
+    print('my_class.__class__.__dict__ =', my_class.__class__.__dict__)
+
+    '''
+        my_class.__dict__ = {}
+        my_class.__class__.__dict__ = {'__module__': '__main__', '__dict__': <attribute '__dict__' of 'MyClass' objects>, '__weakref__': <attribute '__weakref__' of 'MyClass' objects>, '__doc__': None}
+        
+        my_class.__dict__ = {'field': 'One'}
+        my_class.__class__.__dict__ = {'__module__': '__main__', '__dict__': <attribute '__dict__' of 'MyClass' objects>, '__weakref__': <attribute '__weakref__' of 'MyClass' objects>, '__doc__': None}
+        
+        my_class.__dict__ = {}
+        my_class.__class__.__dict__ = {'__module__': '__main__', '__dict__': <attribute '__dict__' of 'MyClass2' objects>, '__weakref__': <attribute '__weakref__' of 'MyClass2' objects>, '__doc__': None}
+    '''
+
+if __name__ == '__main__':
+    main()
---
+++ MyDocs/practice/setattr_prac.py
@@ -0,0 +1,27 @@
+class MyClass(object):
+    class_field = 'One'
+
+    def __init__(self, instance_field):
+        self.instance_field = instance_field
+
+
+def main():
+    my_class = MyClass('Hello')
+    print('my_class.__dict__ =', my_class.__dict__)
+    print('my_class.__class__.__dict__ =', my_class.__class__.__dict__)
+    print()
+
+    my_class.class_field = 'Two'
+    print('my_class.__dict__ =', my_class.__dict__)
+    print('my_class.__class__.__dict__ =', my_class.__class__.__dict__)
+    print('my_class.instance_field =', my_class.instance_field)
+    print('getattr() =', getattr(my_class, 'instance_field', 'Default'))
+    print()
+
+    setattr(my_class, 'instance_field', 'Goodnight')
+    print('my_class.instance_field =', my_class.instance_field)
+    print('getattr() =', getattr(my_class, 'instance_field', 'Default'))
+
+
+if __name__ == '__main__':
+    main()
