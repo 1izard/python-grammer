5880f976e4b0a73c798214b5a21f3f9b091c9fb1
1izard <guitar.kubittake@gmail.com>
2019-02-21 22:47:47 +0900

    update metaclass_note.py and add metaclass_note2.py

--- MyDocs/practice/metaclass_notes.py
+++ MyDocs/practice/metaclass_notes.py
@@ -1,4 +1,41 @@
 
 
+class Field(object):
+    def __init__(self, name):
+        self.name = name
+        self.internal_name = '_' + self.name
+
+    def __get__(self, instance, instance_type):
+        print('Called __get__({}, {}, {})'.format(self, instance, instance_type))
+        if instance is None: return self
+        # getattr()
+        #   - Not use __get__(), so this __get__() doesn't loop.
+        #   - If you access to instance attribution using instance.attribution,
+        #     instance and attribution is specified beforehand;
+        #     instance and attribution is constant.
+        #     Using getattr() enable to specify instance and attribution as variable.
+        #   - In this case, it's useless that Field object has field because of using getattr()
+        return getattr(instance, self.internal_name, '')
+
+    def __set__(self, instance, value):
+        print('Called __set__({}, {}, {})'.format(self, instance, value))
+        setattr(instance, self.internal_name, value)
+
+
+class Customer(object):
+    first_name = Field('first_name')
+    last_name = Field('last_name')
+
+
+def main():
+    customer = Customer()
+    print('customer.__dict__ =', customer.__dict__)
+    print()
+
+    customer.first_name = 'Euclid'
+    print('customer.__dict__ =', customer.__dict__)
+    print('customer.first_name =', customer.first_name)
+
+
 if __name__ == '__main__':
     main()
---
+++ MyDocs/practice/metaclass_notes2.py
@@ -0,0 +1,38 @@
+class Meta(type):
+    def __new__(meta, name, bases, class_dict):
+        for key, value in class_dict.items():
+            if isinstance(value, Field):
+                value.name = key
+                value.internal_name = '_' + key
+        cls = type.__new__(meta, name, bases, class_dict)
+        return cls
+
+
+class Field(object):
+    def __init__(self):
+        self.name = None
+        self.internal_name = None
+
+    def __get__(self, instance, instance_type):
+        if instance is None: return self
+        return getattr(instance, self.internal_name, '')
+
+    def __set__(self, instance, value):
+        setattr(instance, self.internal_name, value)
+
+
+class DatabaseRow(object, metaclass=Meta):
+    pass
+
+
+class BetterCustomer(DatabaseRow):
+    first_name = Field()
+    last_name = Field()
+
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
