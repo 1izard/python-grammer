ba336f41474266664441dcd0be2fdb603298a72a
1izard <guitar.kubittake@gmail.com>
2019-02-21 23:21:41 +0900

    update setattr_prac.py

--- MyDocs/practice/setattr_prac.py
+++ MyDocs/practice/setattr_prac.py
@@ -11,9 +11,21 @@ def main():
     print('my_class.__class__.__dict__ =', my_class.__class__.__dict__)
     print()
 
+    # MyClass.class_field is Not set 'Two'
+    # because code below sets 'Two' into field of my_class instance.
+    # Python newly adds the field into instance __dict__
+    # when the filed doesn't exist in __dict__.
     my_class.class_field = 'Two'
     print('my_class.__dict__ =', my_class.__dict__)
     print('my_class.__class__.__dict__ =', my_class.__class__.__dict__)
+    print()
+
+    # To set class field
+    my_class.__class__.class_field = 'Two'
+    print('my_class.__dict__ =', my_class.__dict__)
+    print('my_class.__class__.__dict__ =', my_class.__class__.__dict__)
+    print()
+
     print('my_class.instance_field =', my_class.instance_field)
     print('getattr() =', getattr(my_class, 'instance_field', 'Default'))
     print()
