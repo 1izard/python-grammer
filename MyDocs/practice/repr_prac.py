d0f9b6fece75ea349e1cb8e971add683b30f53f8
1izard <guitar.kubittake@gmail.com>
2019-03-02 18:36:27 +0900

    edit repr_prac.py

--- MyDocs/practice/repr_prac.py
+++ MyDocs/practice/repr_prac.py
@@ -25,3 +25,32 @@ print(obj.__dict__)
 # BetterClass(1, 2)
 # {'x': 1, 'y': 2}
 
+
+
+class MyClazz(object):
+    def __str__(self):
+        return 'This is __str__()'
+
+
+obj = MyClazz()
+print(obj)
+# This is __str__()
+
+
+# print() expresses __str__() prior to __repr__()
+class MyClazz2(object):
+    # __repr__() returns 'official' string.
+    # Useful for debug.
+    def __repr__(self):
+        return 'This is __repr__()'
+
+    # __str__() returns 'informal' string.
+    # Useful for UI.
+    def __str__(self):
+        return 'This is __str__()'
+
+
+obj = MyClazz2()
+print(obj)
+# This is __str__()
+
