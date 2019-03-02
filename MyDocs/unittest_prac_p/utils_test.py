3931ea15255c245252a27f840d319b4269863e1c
1izard <guitar.kubittake@gmail.com>
2019-03-02 18:35:47 +0900

    add unittest_prac_p

---
+++ MyDocs/unittest_prac_p/utils_test.py
@@ -0,0 +1,32 @@
+from unittest import TestCase, main
+from utils import to_str
+from tempfile import TemporaryDirectory
+
+
+class UtilsTestCase(TestCase):
+    def test_to_str_bytes(self):
+        self.assertEqual('hello', to_str(b'hello'))
+
+    def test_to_str_str(self):
+        self.assertEqual('hello', to_str('hello'))
+
+    def test_to_str_bad(self):
+        self.assertRaises(TypeError, to_str, object())
+
+
+if __name__ == '__main__':
+    main()
+
+# ...
+# ----------------------------------------------------------------------
+# Ran 3 tests in 0.000s
+#
+# OK
+
+
+class MyTest(TestCase):
+    def setUp(self):
+        self.test_dir = TemporaryDirectory()
+
+    def tearDown(self):
+        self.test_dir.cleanup()
